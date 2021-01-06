fda83ef78fd3687370f87c81b8b63dbdb7ecd1d0
Svenja Niehus <svenja.niehus@gmail.com>
2021-01-06 16:11:01 +0100

    gdc projects fetch with TCGA filter in graphQL

--- src/App.js
+++ src/App.js
@@ -1,72 +1,74 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react'
+import './App.css'
+import GdcDataChoice from './GdcDataChoice'
 
 function App() {
-  const [ensgNumber, setensgNumber] = useState("");
-  const [inputValue, setInputValue] = useState("");
-  const [firstSearchResults, setFirstSearchResults] = useState([]);
+	const [ensgNumber, setensgNumber] = useState('')
+	const [inputValue, setInputValue] = useState('')
+	const [firstSearchResults, setFirstSearchResults] = useState([])
 
-  function getHgncId(input) {
-    fetch(`http://rest.genenames.org/search/${input}`, {
-      headers: { Accept: "application/json" },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setFirstSearchResults(res.response.docs);
-      });
-  }
+	function getHgncId(input) {
+		fetch(`http://rest.genenames.org/search/${input}`, {
+			headers: { Accept: 'application/json' },
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				setFirstSearchResults(res.response.docs)
+			})
+	}
 
-  function getEnsgNumber(name) {
-    const url = `http://rest.genenames.org/fetch/hgnc_id/${name}`;
-    fetch(url, {
-      headers: { Accept: "application/json" },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setensgNumber(res.response.docs[0].ensembl_gene_id);
-      });
-  }
+	function getEnsgNumber(name) {
+		const url = `http://rest.genenames.org/fetch/hgnc_id/${name}`
+		fetch(url, {
+			headers: { Accept: 'application/json' },
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				setensgNumber(res.response.docs[0].ensembl_gene_id)
+			})
+	}
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
+	const handleChange = (e) => {
+		setInputValue(e.target.value)
+	}
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(inputValue);
-    getHgncId(inputValue);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(inputValue)
+		getHgncId(inputValue)
+	}
 
-  const handleClick = (e) => {
-    console.log(e.target.value);
-    getEnsgNumber(e.target.value);
-  };
+	const handleClick = (e) => {
+		console.log(e.target.value)
+		getEnsgNumber(e.target.value)
+	}
 
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit} style={{ margin: "60px" }}>
-        <input
-          type="text"
-          placeholder="Gene in any name"
-          onChange={handleChange}
-          value={inputValue}
-        ></input>
-        <button type="submit" style={{ marginLeft: "10px" }}>
-          Submit
-        </button>
-      </form>
-      <ul>
-        {firstSearchResults.map((result) => (
-          <li key={result.hgnc_id}>
-            <button value={result.hgnc_id} onClick={handleClick}>
-              {result.symbol}
-            </button>
-          </li>
-        ))}
-      </ul>
-      {ensgNumber}
-    </div>
-  );
+	return (
+		<div className='App'>
+			<form onSubmit={handleSubmit} style={{ margin: '60px' }}>
+				<input
+					type='text'
+					placeholder='Gene in any name'
+					onChange={handleChange}
+					value={inputValue}
+				></input>
+				<button type='submit' style={{ marginLeft: '10px' }}>
+					Submit
+				</button>
+			</form>
+			<ul>
+				{firstSearchResults.map((result) => (
+					<li key={result.hgnc_id}>
+						<button value={result.hgnc_id} onClick={handleClick}>
+							{result.symbol}
+						</button>
+					</li>
+				))}
+			</ul>
+			{ensgNumber}
+			<GdcDataChoice />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/GdcDataChoice.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+
+export default function GdcDataChoice() {
+	const [gdcProjects, setGdcProjects] = useState([])
+
+	function getGdcProjects() {
+		fetch('https://api.gdc.cancer.gov/v0/graphql', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({
+				query: `
+			  
+			  query PROJECTS_EDGES($filters_1: FiltersArgument) {
+				projects {
+				  hits(first: 1000, filters: $filters_1) {
+					total
+					edges {
+					  node {
+						name
+						project_id
+				  
+					  }
+					}
+				  }
+				}
+			  }
+			`,
+				variables: {
+					filters_1: {
+						op: '=',
+						content: { field: 'project_id', value: ['TCGA*'] },
+					},
+				},
+			}),
+		})
+			.then((res) => res.json())
+			.then((res) => setGdcProjects(res.data.projects.hits.edges))
+	}
+
+	useEffect(() => {
+		getGdcProjects()
+	}, [])
+
+	if (gdcProjects) {
+		console.log(gdcProjects)
+		return (
+			<>
+				<ul>
+					{gdcProjects.map((project) => (
+						<li key={project.node.project_id}>{project.node.name}</li>
+					))}
+				</ul>
+			</>
+		)
+	} else {
+		return <h1>loading available GDC projects...</h1>
+	}
+}
