b7ee930a81bf357c7029262ff9013ccb069325f5
Ana Caballero <ana.caballero@gmx.de>
2021-01-11 14:16:15 +0100

    convert project choices into checkbox elements

--- src/Components/GdcProjectChoice.js
+++ src/Components/GdcProjectChoice.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
 import "../App.css";
 
 export default function GdcProjectChoice() {
   const [gdcProjects, setGdcProjects] = useState([]);
+  const [selectedProjects, setSelectedProjects] = useState([]);
 
   function getGdcProjects() {
     fetch("https://api.gdc.cancer.gov/v0/graphql", {
@@ -45,14 +47,36 @@ export default function GdcProjectChoice() {
     getGdcProjects();
   }, []);
 
+  const handleChange = (e) => {
+    const selection = e.target.name;
+    let previousSelection = selectedProjects;
+
+    if (e.target.checked) {
+      previousSelection.push(selection);
+    } else {
+      const index = selectedProjects.indexOf(selection);
+      previousSelection.splice(index, 1);
+    }
+    setSelectedProjects(previousSelection);
+    console.log(selectedProjects);
+  };
+
   if (gdcProjects) {
     return (
       <>
-        <ul>
+        <Form>
           {gdcProjects.map((project) => (
-            <li key={project.node.project_id}>{project.node.name}</li>
+            //<li key={project.node.project_id}>{project.node.name}</li>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                name={project.node.project_id}
+                label={project.node.name}
+                onChange={handleChange}
+              />
+            </Form.Group>
           ))}
-        </ul>
+        </Form>
       </>
     );
   } else {
