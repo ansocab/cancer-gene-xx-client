2f0e8bed21219d17fea0432545c0eb7668b43fb5
Svenja Niehus <svenja.niehus@gmail.com>
2021-01-14 13:26:08 +0100

    cleanup console.logs

--- src/Components/GdcCategoryChoice.js
+++ src/Components/GdcCategoryChoice.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { Col } from "react-bootstrap";
-import GdcDataTypeChoice from "./GdcDataTypeChoice";
-import "../App.css";
+import React, { useState, useEffect } from 'react'
+import { Col } from 'react-bootstrap'
+import GdcDataTypeChoice from './GdcDataTypeChoice'
+import '../App.css'
 
 export default function GdcCategoryChoice(props) {
-  const [gdcCategories, setGdcCategories] = useState([]);
-  const [uniqueCategories, setUniqueCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState([]);
-  const [showDataType, setShowDataType] = useState(false);
+	const [gdcCategories, setGdcCategories] = useState([])
+	const [uniqueCategories, setUniqueCategories] = useState([])
+	const [selectedCategory, setSelectedCategory] = useState([])
+	const [showDataType, setShowDataType] = useState(false)
 
-  function getGdcCategories() {
-    fetch("https://api.gdc.cancer.gov/v0/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        query: `
+	function getGdcCategories() {
+		fetch('https://api.gdc.cancer.gov/v0/graphql', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({
+				query: `
                 query DataCategoryFileCounts($filters: FiltersArgument) {
                         projects {
                           hits(first: 1000, filters: $filters) {
@@ -36,94 +36,91 @@ export default function GdcCategoryChoice(props) {
                         }
                     } 
                       }`,
-        variables: {
-          filters: {
-            op: "=",
-            content: { field: "project_id", value: props.project },
-          },
-        },
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setGdcCategories(res.data.projects.hits.edges);
-      });
-  }
+				variables: {
+					filters: {
+						op: '=',
+						content: { field: 'project_id', value: props.project },
+					},
+				},
+			}),
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				setGdcCategories(res.data.projects.hits.edges)
+			})
+	}
 
-  useEffect(() => {
-    getGdcCategories();
-  }, []);
+	useEffect(() => {
+		getGdcCategories()
+	}, [])
 
-  useEffect(() => {
-    showCategories();
-  }, [gdcCategories]);
+	useEffect(() => {
+		showCategories()
+	}, [gdcCategories])
 
-  function showCategories() {
-    let helperSet = new Set();
-    gdcCategories.map((category) =>
-      category.node.summary.data_categories.map((subcategory) =>
-        helperSet.add(subcategory.data_category)
-      )
-    );
+	function showCategories() {
+		let helperSet = new Set()
+		gdcCategories.map((category) =>
+			category.node.summary.data_categories.map((subcategory) =>
+				helperSet.add(subcategory.data_category)
+			)
+		)
 
-    setUniqueCategories(Array.from(helperSet));
-  }
+		setUniqueCategories(Array.from(helperSet))
+	}
 
-  const handleChange = (e) => {
-    if (e.target.checked) {
-      setSelectedCategory([...selectedCategory, e.target.id]);
-    } else {
-      setSelectedCategory((prev) =>
-        prev.filter((item) => item !== e.target.id)
-      );
-    }
-  };
+	const handleChange = (e) => {
+		if (e.target.checked) {
+			setSelectedCategory([...selectedCategory, e.target.id])
+		} else {
+			setSelectedCategory((prev) => prev.filter((item) => item !== e.target.id))
+		}
+	}
 
-  useEffect(() => {
-    if (selectedCategory.length !== 0) {
-      setShowDataType(true);
-    } else {
-      setShowDataType(false);
-    }
-    console.log(selectedCategory);
-  }, [selectedCategory]);
+	useEffect(() => {
+		if (selectedCategory.length !== 0) {
+			setShowDataType(true)
+		} else {
+			setShowDataType(false)
+		}
+	}, [selectedCategory])
 
-  return (
-    <>
-      <Col md="6" xl="3">
-        <h3>Categories</h3>
-        {uniqueCategories ? (
-          <fieldset>
-            <div className="form-group">
-              {uniqueCategories.map((category) => (
-                <div key={category} className="custom-control custom-checkbox">
-                  <input
-                    type="checkbox"
-                    className="custom-control-input"
-                    id={category}
-                    onChange={handleChange}
-                  />
-                  <label className="custom-control-label" for={category}>
-                    {category}
-                  </label>
-                </div>
-              ))}
-            </div>
-          </fieldset>
-        ) : (
-          <h1>loading available GDC projects...</h1>
-        )}
-      </Col>
-      <>
-        {showDataType && (
-          <>
-            <GdcDataTypeChoice
-              category={selectedCategory}
-              project={props.project}
-            />
-          </>
-        )}
-      </>
-    </>
-  );
+	return (
+		<>
+			<Col md='6' xl='3'>
+				<h3>Categories</h3>
+				{uniqueCategories ? (
+					<fieldset>
+						<div className='form-group'>
+							{uniqueCategories.map((category) => (
+								<div key={category} className='custom-control custom-checkbox'>
+									<input
+										type='checkbox'
+										className='custom-control-input'
+										id={category}
+										onChange={handleChange}
+									/>
+									<label className='custom-control-label' for={category}>
+										{category}
+									</label>
+								</div>
+							))}
+						</div>
+					</fieldset>
+				) : (
+					<h1>loading available GDC projects...</h1>
+				)}
+			</Col>
+			<>
+				{showDataType && (
+					<>
+						<GdcDataTypeChoice
+							category={selectedCategory}
+							project={props.project}
+						/>
+					</>
+				)}
+			</>
+		</>
+	)
 }
