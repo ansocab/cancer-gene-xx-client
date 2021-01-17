a985113e71729cfa15f313aa36f39f69540f4e7f
Ana Caballero <ana.caballero@gmx.de>
2021-01-17 12:53:03 +0100

    fix data download

--- src/Components/DataDownload.js
+++ src/Components/DataDownload.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 // import download from "downloadjs";
-import untar from 'js-untar'
-import BoxPlot from './BoxPlot'
-import CollapsableCard from './CollapsableCard'
-import Loading from './Loading'
+import untar from "js-untar";
+import BoxPlot from "./BoxPlot";
+import CollapsableCard from "./CollapsableCard";
+import Loading from "./Loading";
+import { merge } from "d3v4";
 
 // const extract = require("extract-zip");
-const pako = require('pako')
+const pako = require("pako");
 
 // fetch everything DONE
 // "unarchive" DONE
@@ -15,61 +16,61 @@ const pako = require('pako')
 // for each ungzipped file, find the correct line of text, save to object with key=filename, value=value of matching
 
 export default function DataDownload(props) {
-	const { ensgNumber } = useParams()
-	const [results, setResults] = useState([])
-	const [boxPlotValues, setBoxPlotValues] = useState([])
-	const [caseIds, setCaseIds] = useState([])
-	const [mergedData, setMergedData] = useState([])
+  const { ensgNumber } = useParams();
+  const [results, setResults] = useState([]);
+  const [boxPlotValues, setBoxPlotValues] = useState([]);
+  const [caseIds, setCaseIds] = useState([]);
+  const [mergedData, setMergedData] = useState([]);
 
-	const unarchive = async function (files) {
-		let unzippedFiles = []
-		try {
-			await untar(files).progress(function (extractedFile) {
-				const newFileOutput = pako.ungzip(extractedFile.buffer, {
-					to: 'string',
-				})
-				unzippedFiles.push({ [extractedFile.name]: newFileOutput })
-			})
-		} catch (err) {}
-		return unzippedFiles
-	}
+  const unarchive = async function (files) {
+    let unzippedFiles = [];
+    try {
+      await untar(files).progress(function (extractedFile) {
+        const newFileOutput = pako.ungzip(extractedFile.buffer, {
+          to: "string",
+        });
+        unzippedFiles.push({ [extractedFile.name]: newFileOutput });
+      });
+    } catch (err) {}
+    return unzippedFiles;
+  };
 
-	function fetchDataValues() {
-		fetch('https://api.gdc.cancer.gov/data?tarfile', {
-			method: 'post',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				ids: props.idArray,
-			}),
-		})
-			.then((r) => r.arrayBuffer())
-			.then((r) => unarchive(r))
-			.then((files) => {
-				setResults(
-					files.map((file) => {
-						var fileLines = Object.values(file)[0].split('\n')
-						for (var i = 0; i < fileLines.length; i++) {
-							var y = fileLines[i].split('\t')
-							if (y[0].includes(ensgNumber)) {
-								return { [Object.keys(file)[0]]: y[1] }
-							}
-						}
-						return { [Object.keys(file)[0]]: 'no match' }
-					})
-				)
-			})
-	}
+  function fetchDataValues() {
+    fetch("https://api.gdc.cancer.gov/data?tarfile", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ids: props.idArray,
+      }),
+    })
+      .then((r) => r.arrayBuffer())
+      .then((r) => unarchive(r))
+      .then((files) => {
+        setResults(
+          files.map((file) => {
+            var fileLines = Object.values(file)[0].split("\n");
+            for (var i = 0; i < fileLines.length; i++) {
+              var y = fileLines[i].split("\t");
+              if (y[0].includes(ensgNumber)) {
+                return { [Object.keys(file)[0]]: y[1] };
+              }
+            }
+            return { [Object.keys(file)[0]]: "no match" };
+          })
+        );
+      });
+  }
 
-	function fetchCaseIds() {
-		fetch('https://api.gdc.cancer.gov/v0/graphql', {
-			method: 'post',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				query: `
+  function fetchCaseIds() {
+    fetch("https://api.gdc.cancer.gov/v0/graphql", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
 				query additionalData($filters: FiltersArgument){
 				  viewer {
 					repository {
@@ -95,38 +96,39 @@ export default function DataDownload(props) {
 				
 				}
 				  }}`,
-				variables: {
-					filters: {
-						op: '=',
-						content: {
-							field: 'file_id',
-							value: props.idArray,
-						},
-					},
-				},
-			}),
-		})
-			.then((r) => r.json())
-			.then((r) => {
-				var idArray = []
-				r.data.viewer.repository.files.hits.edges.map((middlePart) =>
-					middlePart.node.cases.hits.edges.map((id) =>
-						idArray.push(id.node.case_id)
-					)
-				)
-				setCaseIds(idArray)
-				fetchCaseData()
-			})
-	}
+        variables: {
+          filters: {
+            op: "=",
+            content: {
+              field: "file_id",
+              value: props.idArray,
+            },
+          },
+        },
+      }),
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        var idArray = [];
+        r.data.viewer.repository.files.hits.edges.map((middlePart) =>
+          middlePart.node.cases.hits.edges.map((id) =>
+            idArray.push(id.node.case_id)
+          )
+        );
+        setCaseIds(idArray);
+        fetchCaseData(idArray);
+      });
+  }
 
-	function fetchCaseData() {
-		fetch('https://api.gdc.cancer.gov/v0/graphql', {
-			method: 'post',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				query: `
+  function fetchCaseData(ids) {
+    console.log(results);
+    fetch("https://api.gdc.cancer.gov/v0/graphql", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
 			query additionalData($filters: FiltersArgument){
 			  viewer {
 				repository {
@@ -158,88 +160,93 @@ export default function DataDownload(props) {
 			  }
 			}
 			  }}`,
-				variables: {
-					filters: {
-						op: '=',
-						content: {
-							field: 'case_id',
-							value: caseIds,
-						},
-					},
-				},
-			}),
-		})
-			.then((r) => r.json())
-			.then((r) => {
-				let arrayOfDataObjects = []
-				r.data.viewer.repository.cases.hits.edges.map(
-					(middlePart, index) =>
-						(arrayOfDataObjects[index] = {
-							file_id: props.idArray[index],
-							gene_value: Object.values(results[index])[0],
-							case_id: caseIds[index],
-							days_to_death: middlePart.node.demographic.days_to_death,
-							gender: middlePart.node.demographic.gender,
-							vital_status: middlePart.node.demographic.vital_status,
-							tumor_grade:
-								middlePart.node.diagnoses.hits.edges[0].node.tumor_grade,
-							tumor_stage:
-								middlePart.node.diagnoses.hits.edges[0].node.tumor_stage,
-						})
-				)
-				setMergedData(arrayOfDataObjects)
-			})
-	}
+        variables: {
+          filters: {
+            op: "=",
+            content: {
+              field: "case_id",
+              value: ids,
+            },
+          },
+        },
+      }),
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        let arrayOfDataObjects = [];
+        r.data.viewer.repository.cases.hits.edges.map(
+          (middlePart, index) =>
+            (arrayOfDataObjects[index] = {
+              file_id: props.idArray[index],
+              gene_value: Object.values(results[index])[0],
+              case_id: ids[index],
+              days_to_death: middlePart.node.demographic.days_to_death,
+              gender: middlePart.node.demographic.gender,
+              vital_status: middlePart.node.demographic.vital_status,
+              tumor_grade:
+                middlePart.node.diagnoses.hits.edges[0].node.tumor_grade,
+              tumor_stage:
+                middlePart.node.diagnoses.hits.edges[0].node.tumor_stage,
+            })
+        );
+        console.log("array of data objects: " - arrayOfDataObjects);
+        setMergedData(arrayOfDataObjects);
+      });
+  }
 
-	useEffect(() => {
-		fetchDataValues()
-		fetchCaseIds()
-	}, [])
+  useEffect(() => {
+    fetchDataValues();
+  }, []);
 
-	useEffect(() => {
-		getBoxPlotData()
-	}, [mergedData])
+  useEffect(() => {
+    results.length && fetchCaseIds();
+  }, [results]);
 
-	function getBoxPlotData() {
-		let helperArray = []
-		results.map((r) => helperArray.push(Object.values(r)[0]))
-		setBoxPlotValues(helperArray.map((i) => Number(i)))
-	}
+  useEffect(() => {
+    console.log("merged data: " + mergedData);
+    getBoxPlotData();
+  }, [mergedData]);
 
-	return (
-		<div>
-			<CollapsableCard title={`Results for ${ensgNumber}`}>
-				<table class='table table-hover'>
-					<thead>
-						<tr class='table-primary'>
-							<th scope='col'>File ID</th>
-							<th scope='col'>FPKM of {ensgNumber}</th>
-							<th scope='col'>Case ID</th>
-							<th scope='col'>Vital Status</th>
-							<th scope='col'>Days to Death</th>
-							<th scope='col'>Gender</th>
-							<th scope='col'>Tumor Grade</th>
-							<th scope='col'>Tumor Stage</th>
-						</tr>
-					</thead>
-					<tbody>
-						{mergedData.length &&
-							mergedData.map((r) => (
-								<tr>
-									<td>{r.file_id}</td>
-									<td>{r.gene_value}</td>
-									<td>{r.case_id}</td>
-									<td>{r.vital_status}</td>
-									<td>{r.days_to_death}</td>
-									<td>{r.gender}</td>
-									<td>{r.tumor_grade}</td>
-									<td>{r.tumor_stage}</td>
-								</tr>
-							))}
-					</tbody>
-				</table>
-			</CollapsableCard>
-			{boxPlotValues.length && <BoxPlot boxPlotValues={boxPlotValues} />}
-		</div>
-	)
+  function getBoxPlotData() {
+    let helperArray = [];
+    results.map((r) => helperArray.push(Object.values(r)[0]));
+    setBoxPlotValues(helperArray.map((i) => Number(i)));
+  }
+
+  return (
+    <div>
+      <CollapsableCard title={`Results for ${ensgNumber}`}>
+        <table class="table table-hover">
+          <thead>
+            <tr class="table-primary">
+              <th scope="col">File ID</th>
+              <th scope="col">FPKM of {ensgNumber}</th>
+              <th scope="col">Case ID</th>
+              <th scope="col">Vital Status</th>
+              <th scope="col">Days to Death</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Tumor Grade</th>
+              <th scope="col">Tumor Stage</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mergedData.length &&
+              mergedData.map((r) => (
+                <tr>
+                  <td>{r.file_id}</td>
+                  <td>{r.gene_value}</td>
+                  <td>{r.case_id}</td>
+                  <td>{r.vital_status}</td>
+                  <td>{r.days_to_death}</td>
+                  <td>{r.gender}</td>
+                  <td>{r.tumor_grade}</td>
+                  <td>{r.tumor_stage}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </CollapsableCard>
+      {boxPlotValues.length && <BoxPlot boxPlotValues={boxPlotValues} />}
+    </div>
+  );
 }
